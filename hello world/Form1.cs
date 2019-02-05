f906727aa0fe8fe44173e3575ce0ad0fa9c6a235
rc0035391 <rc0035391@CRO-24-02>
2019-02-05 09:50:18 +0000

    added textbox

--- hello world/Form1.cs
+++ hello world/Form1.cs
@@ -19,7 +19,12 @@ namespace hello_world
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("allo");
+            MessageBox.Show("allo " + txtBxName.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
